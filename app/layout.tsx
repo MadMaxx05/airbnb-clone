0ddd16fb8cc3b4eecde392d0b9e1c66fb7999928
Maxim Kucherenko <maximkucherenkowork@gmail.com>
2023-06-20 00:24:06 +0300

    [feature] Add Auth UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,30 @@
-import './globals.css';
-import { Inter } from 'next/font/google';
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-import Navbar from './components/navbar/Navbar';
+import Navbar from "./components/navbar/Navbar";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Airbnb Clone',
-  description: 'Made with Next.js 13',
+  title: "Airbnb Clone",
+  description: "Made with Next.js 13",
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
         {children}
       </body>
     </html>
-  )
+  );
 }
