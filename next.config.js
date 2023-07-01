1dd4247e316b2cffd2f306e1165a3f52ec0191e8
Maxim Kucherenko <maximkucherenkowork@gmail.com>
2023-07-01 19:55:26 +0300

    [feature] Add Google and Github authorization

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["bcrypt"],
   },
+  images: {
+    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+  },
 };
 
 module.exports = nextConfig;
